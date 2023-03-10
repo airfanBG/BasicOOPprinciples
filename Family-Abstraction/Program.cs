c1f93e920dde2ab24f1c230b06fdd830050aab60
daniel.damyanov <daniel.damyanov@primeholding.com>
2023-03-10 08:52:11 +0200

    .

---
+++ Family-Abstraction/Program.cs
@@ -0,0 +1,156 @@
+﻿//https://dotnettutorials.net/lesson/abstraction-csharp-realtime-example/
+
+public class Startup
+{
+    public static void Main()
+    {
+        List<Person> people = new List<Person>();
+        List<string> ties = new List<string>();
+
+        string searchNameOrBirthday = Console.ReadLine();
+        string inputLine = Console.ReadLine();
+        while (inputLine != "End")
+        {
+            string[] parts = inputLine.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts.Length == 1)
+            {
+                string[] args = inputLine.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                string name = $"{args[0]} {args[1]}";
+                string birthday = args[2];
+                Person person = new Person(name, birthday);
+                people.Add(person);
+            }
+            else
+            {
+                ties.Add(inputLine);
+            }
+            inputLine = Console.ReadLine();
+        }
+
+        foreach (string tie in ties)
+        {
+            Person parent = new Person();
+            Person child = new Person();
+
+            string[] parts = tie.Split(new[] { " - " }, StringSplitOptions.RemoveEmptyEntries);
+            if (parts[0].Contains("/") && parts[1].Contains("/"))
+            {
+                string parentBirthday = parts[0];
+                string childBirthday = parts[1];
+
+                parent = people.First(p => p.Birthday == parentBirthday);
+                child = people.First(p => p.Birthday == childBirthday);
+            }
+            else if (parts[0].Contains("/") || parts[1].Contains("/"))
+            {
+                if (parts[0].Contains("/"))
+                {
+                    string birthday = parts[0];
+                    string name = parts[1];
+
+                    parent = people.First(p => p.Birthday == birthday);
+                    child = people.First(p => p.Name == name);
+                }
+                else
+                {
+                    string birthday = parts[1];
+                    string name = parts[0];
+
+                    parent = people.First(p => p.Name == name);
+                    child = people.First(p => p.Birthday == birthday);
+                }
+            }
+            else
+            {
+                string parentName = parts[0];
+                string childName = parts[1];
+
+                parent = people.First(p => p.Name == parentName);
+                child = people.First(p => p.Name == childName);
+            }
+
+            if (!parent.Children.Contains(child))
+            {
+                parent.Children.Add(child);
+            }
+            if (!parent.Parents.Contains(parent))
+            {
+                child.Parents.Add(parent);
+            }
+        }
+
+        Person searchPerson = new Person();
+        if (searchNameOrBirthday.Contains("/"))
+        {
+            searchPerson = people.First(p => p.Birthday == searchNameOrBirthday);
+        }
+        else
+        {
+            searchPerson = people.First(p => p.Name == searchNameOrBirthday);
+        }
+
+        Console.WriteLine($"{searchPerson.Name} {searchPerson.Birthday}");
+        Console.WriteLine($"Parents:");
+        if (searchPerson.Parents.Count > 0)
+        {
+            foreach (Person parent in searchPerson.Parents)
+            {
+                Console.WriteLine($"{parent.Name} {parent.Birthday}");
+            }
+        }
+        Console.WriteLine($"Children:");
+        if (searchPerson.Children.Count > 0)
+        {
+            foreach (Person child in searchPerson.Children)
+            {
+                Console.WriteLine($"{child.Name} {child.Birthday}");
+            }
+        }
+    }
+}
+
+public class Person
+{
+    private string name;
+    private string birthday;
+    private List<Person> parents;
+    private List<Person> children;
+
+    public Person()
+    {
+        this.parents = new List<Person>();
+        this.children = new List<Person>();
+    }
+
+    public Person(string name, string birthday)
+    {
+        this.Name = name;
+        this.Birthday = birthday;
+        this.Parents = new List<Person>();
+        this.Children = new List<Person>();
+    }
+
+    public string Name
+    {
+        get { return this.name; }
+        set { this.name = value; }
+    }
+
+    public string Birthday
+    {
+        get { return this.birthday; }
+        set { this.birthday = value; }
+    }
+
+    public List<Person> Parents
+    {
+        get { return this.parents; }
+        set { this.parents = value; }
+    }
+
+    public List<Person> Children
+    {
+        get { return this.children; }
+        set { this.children = value; }
+    }
+}
---
+++ Radio-Inheritance/Program.cs
@@ -0,0 +1,132 @@
+﻿//https://dotnettutorials.net/lesson/inheritance-c-sharp/
+
+public class Startup
+{
+    public static void Main()
+    {
+        List<Song> songs = new List<Song>();
+        int number = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < number; i++)
+        {
+            string[] inputParts = Console.ReadLine().Split(new[] { ';' }, StringSplitOptions.RemoveEmptyEntries);
+            try
+            {
+                if (inputParts.Length != 3)
+                {
+                    throw new InvalidDataException("Some msg");
+                }
+                ParseSong(songs, inputParts);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        PrintTotalTime(songs);
+    }
+
+    private static void ParseSong(List<Song> songs, string[] inputParts)
+    {
+        string[] time = inputParts[2].Split(new[] { ':' }, StringSplitOptions.RemoveEmptyEntries);
+        int minutes;
+        int seconds;
+        if (int.TryParse(time[0], out minutes) && int.TryParse(time[1], out seconds))
+        {
+            Song song = new Song(inputParts[0], inputParts[1], minutes, seconds);
+            songs.Add(song);
+            Console.WriteLine("Song added.");
+        }
+        else
+        {
+            throw new InvalidDataException("Some msg");
+        }
+    }
+
+    private static void PrintTotalTime(List<Song> songs)
+    {
+        long totalSecond = songs.Sum(m => m.Minutes * 60) + songs.Sum(s => s.Seconds);
+        TimeSpan ts = TimeSpan.FromSeconds(totalSecond);
+        Console.WriteLine($"Songs added: {songs.Count}");
+        Console.WriteLine($"Playlist length: {ts.Hours}h {ts.Minutes}m {ts.Seconds}s");
+    }
+}
+
+
+public class Song
+{
+    private const int ArtistMinNameLength = 3;
+    private const int ArtistMaxNameLength = 20;
+    private const int SongMinNameLength = 3;
+    private const int SongMaxNameLength = 30;
+    private const int MinutesMin = 0;
+    private const int MinutesMax = 14;
+    private const int SecondsMin = 0;
+    private const int SecondsMax = 59;
+
+    private string artistName;
+    private string songName;
+    private int minutes;
+    private int seconds;
+
+    public Song(string artistName, string songName, int minutes, int seconds)
+    {
+        this.ArtistName = artistName;
+        this.SongName = songName;
+        this.Minutes = minutes;
+        this.Seconds = seconds;
+    }
+
+    public int Seconds
+    {
+        get { return this.seconds; }
+        set
+        {
+            if (value < SecondsMin || value > SecondsMax)
+            {
+                throw new InvalidDataException("Some msg");
+            }
+            this.seconds = value;
+        }
+    }
+
+    public int Minutes
+    {
+        get { return this.minutes; }
+        set
+        {
+            if (value < MinutesMin || value > MinutesMax)
+            {
+                throw new InvalidDataException("Some msg");
+            }
+            this.minutes = value;
+        }
+    }
+
+    public string SongName
+    {
+        get { return this.songName; }
+        set
+        {
+            if (value.Length < SongMinNameLength || value.Length > SongMaxNameLength)
+            {
+                throw new InvalidDataException("Some msg");
+            }
+            this.songName = value;
+        }
+    }
+
+    public string ArtistName
+    {
+        get { return this.artistName; }
+        set
+        {
+            if (value.Length < ArtistMinNameLength || value.Length > ArtistMaxNameLength)
+            {
+                throw new InvalidDataException("Some msg");
+            }
+            this.artistName = value;
+        }
+    }
+}
